1d477d0503f600871ffce6771e1f7717a2d65211
unknown <anton_semenchuk@rambler.ru>
2024-06-27 17:26:59 +0300

    Polygon Area Calculator project

---
+++ Polygon Area Calculator.py
@@ -0,0 +1,78 @@
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def set_width(self, width):
+        self.width = width
+
+    def set_height(self, height):
+        self.height = height
+
+    def get_area(self):
+        return self.width * self.height
+
+    def get_perimeter(self):
+        return 2 * self.width + 2 * self.height
+
+    def get_diagonal(self):
+        return (self.width ** 2 + self.height ** 2) ** .5
+
+    def get_picture(self):
+        rec = ''
+        if self.height > 50 or self.width > 50:
+            return  "Too big for picture."
+
+        for i in range(self.height):
+            for j in range(self.width):
+                rec += "*"
+            rec += '\n'
+        return rec
+
+    def get_amount_inside(self, other):
+        amount = 0
+        amount_h = int(self.height / other.height)
+        amount_w = int(self.width / other.width)
+        return amount + amount_h*amount_w
+
+    def __str__(self):
+        return f'Rectangle(width={self.width}, height={self.height})'
+
+
+class Square(Rectangle):
+    def __init__(self, side):
+        super().__init__(side, side)
+
+    def set_side(self, side):
+        self.width = side
+        self.height = side
+    
+    def set_width(self, side):
+        self.width = side
+        self.height = side
+
+    def set_height(self, side):
+        self.width = side
+        self.height = side
+
+    def __str__(self):
+        return f'Square(side={self.width})'
+
+
+rect = Rectangle(10, 5)
+print(rect.get_area())
+rect.set_height(3)
+print(rect.get_perimeter())
+print(rect)
+print(rect.get_picture())
+
+sq = Square(9)
+print(sq.get_area())
+sq.set_side(4)
+print(sq.get_diagonal())
+print(sq)
+print(sq.get_picture())
+
+rect.set_height(8)
+rect.set_width(16)
+print(rect.get_amount_inside(sq))
